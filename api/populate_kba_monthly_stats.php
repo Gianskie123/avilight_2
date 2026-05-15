fe3d9e55d066919bc6804a7c068ba3390e9d57cc
GiGi <giancarloregalado14@gmail.com>
2026-05-15 17:26:36 +0800

    For repopulation of kba_pa_monthly_stats

--- api/populate_kba_monthly_stats.php
+++ api/populate_kba_monthly_stats.php
@@ -33,13 +33,6 @@ if (!KPA_CLI) {
 
     require_once __DIR__ . '/../includes/auth.php';
 
-    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-        ob_end_clean();
-        http_response_code(405);
-        echo json_encode(['success' => false, 'error' => 'Method not allowed']);
-        exit;
-    }
-
     if (!is_logged_in()) {
         ob_end_clean();
         http_response_code(401);
